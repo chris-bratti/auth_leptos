24c90f84e1146455df1cc8a82c265e73d10a2b04
Rhys Bratti <rhysbratti@gmail.com>
2024-04-30 13:08:49 -0400

    Removed unecessary test

--- src/server/helpers.rs
+++ src/server/helpers.rs
@@ -15,16 +15,3 @@ pub fn get_env_variable(variable: &str) -> Option<String> {
         }
     }
 }
-
-#[cfg(test)]
-pub mod test_helpers {
-    use crate::server::helpers::get_env_variable;
-
-    #[test]
-    pub fn test_env_variables() {
-        // Ensures the necessary env variables are present
-        assert!(get_env_variable("FROM_EMAIL").is_some());
-        assert!(get_env_variable("SMTP_KEY").is_some());
-        assert!(get_env_variable("ENCRYPTION_KEY").is_some());
-    }
-}
