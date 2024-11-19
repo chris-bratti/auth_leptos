c5b4a3abd098bda4939cfefe5604ef38eab29161
Chris Bratti <148360588+chris-bratti@users.noreply.github.com>
2024-11-19 16:17:02 -0500

    Custom db names (#26)
    * Adding option to customize the database name
    
    * Added in the ability to use custom database name, updated readme, added new test
    
    * Adding in log key to fix github action failure
    
    * Fixing bug that overwrote environment variable in github action

--- init-db.sh
+++ init-db.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# Creates database "daily" if it doesn't already exist
-echo "Creating databse"
-if psql -h leptos_postgres -U master -tc "SELECT 1 FROM pg_database WHERE datname = 'daily'" | grep -q 1; then
+# Creates database if it doesn't already exist
+echo "Creating databse ${DATABASE_NAME}"
+if psql -h leptos_postgres -U master -tc "SELECT 1 FROM pg_database WHERE datname = '${DATABASE_NAME}'" | grep -q 1; then
     echo "Database already exists"
 else
-    psql -h leptos_postgres -U master -c "CREATE DATABASE daily"
+    psql -h leptos_postgres -U master -c "CREATE DATABASE ${DATABASE_NAME}"
 fi
 
-if psql -h leptos_postgres -U master -b daily -tc  "SELECT 1 from information_schema.tables where table_name = 'users'" | grep -q 1; then
+if psql -h leptos_postgres -U master -b ${DATABASE_NAME} -tc "SELECT 1 from information_schema.tables where table_name = 'users'" | grep -q 1; then
     echo "Tables already exist"
 else
-    psql -h leptos_postgres -U master -b daily -f init.sql
+    psql -h leptos_postgres -U master -b ${DATABASE_NAME} -f init.sql
 fi
 
 echo "Starting the application..."
