405af489c4574e6d56633c42f95ead01767f2d5e
Rhys Bratti <rhysbratti@gmail.com>
2024-11-19 13:26:09 -0500

    Adding option to customize the database name

--- init-db.sh
+++ init-db.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-# Creates database "daily" if it doesn't already exist
+# Creates database if it doesn't already exist
+db=${DATABASE_NAME}
 echo "Creating databse"
-if psql -h leptos_postgres -U master -tc "SELECT 1 FROM pg_database WHERE datname = 'daily'" | grep -q 1; then
+if psql -h leptos_postgres -U master -tc "SELECT 1 FROM pg_database WHERE datname = '$db'" | grep -q 1; then
     echo "Database already exists"
 else
-    psql -h leptos_postgres -U master -c "CREATE DATABASE daily"
+    psql -h leptos_postgres -U master -c "CREATE DATABASE $db"
 fi
 
-if psql -h leptos_postgres -U master -b daily -tc  "SELECT 1 from information_schema.tables where table_name = 'users'" | grep -q 1; then
+if psql -h leptos_postgres -U master -b $db -tc "SELECT 1 from information_schema.tables where table_name = 'users'" | grep -q 1; then
     echo "Tables already exist"
 else
-    psql -h leptos_postgres -U master -b daily -f init.sql
+    psql -h leptos_postgres -U master -b $db -f init.sql
 fi
 
 echo "Starting the application..."
